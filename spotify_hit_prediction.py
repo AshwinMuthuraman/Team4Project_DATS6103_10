757457f75fedea688dc40aeb8589845d51166854
AshwinMuthuraman <46164303+AshwinMuthuraman@users.noreply.github.com>
2023-11-20 10:44:32 -0500

    Update spotify_hit_prediction.py

--- spotify_hit_prediction.py
+++ spotify_hit_prediction.py
@@ -21,3 +21,23 @@ print(f'The shape of the dataset is {df.shape}')
 
 # data types of columns
 print(df.dtypes)
+
+#Checking for duplicate elements
+
+def extract_id(text):
+    return text.split(':')[-1]  
+df['uri'] = df['uri'].apply(extract_id)
+
+print(df['uri'].nunique(),) 
+print(df['uri'].value_counts())
+print(df['uri'].value_counts().unique())
+
+duplicate_values = df['uri'].value_counts()==2
+duplicate_index = duplicate_values[duplicate_values]
+print(duplicate_index.value_counts,  duplicate_index.shape) 
+
+duplicate_index  = duplicate_index.index
+duplicate_index = duplicate_index.tolist()
+print(duplicate_index)
+
+remove_duplicate_index = df[df.duplicated(subset='uri', keep=False)].index.tolist() 
