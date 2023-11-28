222c4f94c97592d0d6a1f5335cbfbeed7fdaa537
AshwinMuthuraman <46164303+AshwinMuthuraman@users.noreply.github.com>
2023-11-28 04:05:18 -0500

    Update spotify_hit_prediction.py

--- spotify_hit_prediction.py
+++ spotify_hit_prediction.py
@@ -41,3 +41,41 @@ duplicate_index = duplicate_index.tolist()
 print(duplicate_index)
 
 remove_duplicate_index = df[df.duplicated(subset='uri', keep=False)].index.tolist() 
+
+# cleaned dataset after removing duplicate recors
+df.drop(remove_duplicate_index,axis=0,inplace=True)
+print(df.shape)
+
+#removing the columns which we dont need
+df = df.drop(['track', 'artist', 'uri'], axis=1)
+
+# understanding the ratio of target values to have no bias in the classifier
+print(df.target.value_counts(normalize=True))
+colors = ["#3498db", "#e74c3c"]  
+
+sns.countplot(x='target', data=df, hue='target', palette=colors, legend=False)
+plt.show()
+
+# understanding the distribution of all features
+fig, ax = plt.subplots(5, 3, figsize=(20, 20))
+
+def hist_plot(axis, data, variable, binsnum=10, color='r'):
+    axis.hist(data[variable], bins=binsnum, color=color)
+    axis.set_title(f'{variable.capitalize()} Histogram')
+
+column_features = df.columns.tolist() 
+column_features = column_features[:min(len(column_features), 15)]  
+
+for i, j in enumerate(column_features):
+    row = i // 3  
+    col = i % 3   
+    hist_plot(ax[row, col], df, j)
+
+if len(column_features) < 15:
+    for i in range(len(column_features), 15):
+        row = i // 3
+        col = i % 3
+        ax[row, col].axis('off')
+
+plt.tight_layout()
+plt.show()
