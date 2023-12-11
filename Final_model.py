524c21bc91102a49d0a5b035d3e72309b0f4acd9
Kanishk Goel <46555737+KanishkGoel1999@users.noreply.github.com>
2023-12-10 21:57:40 -0500

    Update Final_model.py

--- Final_model.py
+++ Final_model.py
@@ -184,6 +184,7 @@ plt.show()
 dance_hit = df[df['target'] ==1]['danceability'].mean()
 print("The mean of danceability of songs that were hits", dance_hit)
 
+
 # Logistic Regression
 
 import pandas as pd
@@ -227,135 +228,223 @@ print(conf_matrix)
 # Regression Ended
 
 
-# Decision Trees Start
-from sklearn.model_selection import train_test_split
+## DECISION TREES ##
+
+# %%
+# Importing all required libraries
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
 from sklearn.tree import DecisionTreeClassifier
-from  sklearn import tree # for visualization
+from sklearn import tree # for visualization
+from sklearn.metrics import roc_curve, auc, precision_recall_curve, average_precision_score, precision_score, accuracy_score, f1_score, confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+
 import warnings
 warnings.filterwarnings('ignore')
 
+#%%
+# Function to print metrics
+def print_metric(y_pred, y_test):
+    acc = accuracy_score(y_test, y_pred)
+    pre = precision_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+    print("The accuracy score for the model is: {}".format(round(acc, 2)))
+    print("The precision for the model is: {}".format(round(pre,2)))
+    print("The f1 score for the model is: {}".format(round(f1,2)))
 
-#Define dependent & independent features
+    return acc, pre, f1
+
+# %%
+# Define dependent & independent features
 dep_feature = df.iloc[:,-1]
 indep_features = df.iloc[:,0:15]
 
-#Train test split
+# Train test split
 X_train, X_test, y_train, y_test = train_test_split(indep_features, dep_feature, test_size=0.2, random_state=42)
 X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.1, random_state=42)
+
+#%%
+# Generic model 
 model0 = DecisionTreeClassifier()
 model0.fit(X_train, y_train)
-print("The max depth of decision tree is {}".format(model0.tree_.max_depth))
+y_pred=model0.predict(X_test)
+print("The maximum depth of decision tree is {}".format(model0.tree_.max_depth))
+model0_acc, model0_pre, model0_f1 = print_metric(y_pred, y_test)
+tree.plot_tree(model0)
 
+
+#%%
 # Using max depth
 hyperparameter_depth = 0
 diff=5
-#validation_score=list()
-#training_score=list()
+
 for d in range(1, 23):
     model1 = DecisionTreeClassifier(max_depth= d, random_state=42)
     model1.fit(X_train, y_train)
     tr = model1.score(X_train, y_train)
     val = model1.score(X_val, y_val)
-    #training_score.append(tr)
-    #validation_score.append(val)
     if tr-val < diff:
         diff = tr-val
         hyperparameter_depth=d
 
-print(hyperparameter_depth)
+print("The depth with maximum accuracy is {}".format(hyperparameter_depth))
 
-final_model1 = DecisionTreeClassifier(max_depth=hyperparameter_depth, random_state=42)
-final_model1.fit(X_train, y_train)
-
-from sklearn import tree
-plt.figure(figsize=(80,80))
-tree.plot_tree(final_model1, feature_names=X_train.columns, filled=True)
-
-from sklearn.metrics import accuracy_score, confusion_matrix
+model1 = DecisionTreeClassifier(max_depth=hyperparameter_depth, random_state=42)
+model1.fit(X_train, y_train)
 y_pred = model1.predict(X_test)
-model_accuracy = accuracy_score(y_test, y_pred)
-confusion_matrix(y_test, y_pred)
+model1_acc, model1_pre, model1_f1 = print_metric(y_pred, y_test)
+
+# Printing tree with depth = 3
+plt.figure(figsize=(80,80))
+tree.plot_tree(model1, feature_names=X_train.columns, filled=True)
+
+# Print confusion matrix
+cnf=confusion_matrix(y_test, y_pred)
+plt.figure(figsize=(6, 4))
+sns.heatmap(cnf, annot=True, fmt="d", cmap="Blues", cbar=False, 
+            xticklabels=["Predicted 0", "Predicted 1"], 
+            yticklabels=["Actual 0", "Actual 1"])
+plt.title("Confusion Matrix")
+plt.show()
+
+# ROC Curve
+fpr, tpr, thresholds = roc_curve(y_test, y_pred)
+roc_auc = auc(fpr, tpr)
+plt.figure(figsize=(8, 6))
+plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC Curve (AUC = {roc_auc:.2f})')
+plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic (ROC) Curve')
+plt.legend(loc="lower right")
+plt.show()
+
+# Precision-Recall Curve
+precision, recall, thresholds = precision_recall_curve(y_test, y_pred)
+avg_precision = average_precision_score(y_test, y_pred)
+plt.figure(figsize=(8, 6))
+plt.plot(recall, precision, color='darkorange', lw=2, label=f'Precision-Recall Curve (AP = {avg_precision:.2f})')
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.title('Precision-Recall Curve')
+plt.legend(loc="lower right")
+plt.show()
 
 
+# %%
+# We can do this using any of these hyperparameters:
+# max_leaf_nodes
+# max_features
+# min_sample_split
+# min_sample_leaf
+# min_impurity_decrease  
+# bootstrap
 
-# Using max leaf nodes
-model2 = DecisionTreeClassifier(max_leaf_nodes=30, random_state=42)
+
+# %%
+# Random Forest
+model2 = RandomForestClassifier(n_estimators=50, n_jobs=-1)
 model2.fit(X_train, y_train)
-print(model2.score(X_train, y_train))
-print(model2.score(X_val, y_val))
 
-tree.plot_tree(model2)
+y_pred = model2.predict(X_test)
+model2_acc, model2_pre, model2_f1 = print_metric(y_test, y_pred)
+
+plt.figure(figsize=(80,80))
+tree.plot_tree(model2, feature_names=X_train.columns, filled=True)
+
+cnf=confusion_matrix(y_test, y_pred)
+
+plt.figure(figsize=(6, 4))
+sns.heatmap(cnf, annot=True, fmt="d", cmap="Blues", cbar=False, 
+            xticklabels=["Predicted 0", "Predicted 1"], 
+            yticklabels=["Actual 0", "Actual 1"])
+plt.title("Confusion Matrix")
+plt.show()
+
+# ROC Curve
+fpr, tpr, thresholds = roc_curve(y_test, y_pred)
+roc_auc = auc(fpr, tpr)
+plt.figure(figsize=(8, 6))
+plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC Curve (AUC = {roc_auc:.2f})')
+plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic (ROC) Curve')
+plt.legend(loc="lower right")
+plt.show()
+
+# Precision-Recall Curve
+precision, recall, thresholds = precision_recall_curve(y_test, y_pred)
+avg_precision = average_precision_score(y_test, y_pred)
+plt.figure(figsize=(8, 6))
+plt.plot(recall, precision, color='darkorange', lw=2, label=f'Precision-Recall Curve (AP = {avg_precision:.2f})')
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.title('Precision-Recall Curve')
+plt.legend(loc="lower right")
+plt.show()
 
 
-print(model1)
+# %%
+# Hyperparameter tuning using random search cross validation and random forests
 
-from sklearn.ensemble import RandomForestClassifier
-model3 = RandomForestClassifier(n_estimators=50, n_jobs=-1)
-model3.fit(X_train, y_train)
-print(model3.score(X_train, y_train))
-print(model3.score(X_val, y_val))
- 
-
-# Try getting all decision trees
-
-# Using max_features
-for mf in [None, 'log2', 'sqrt']:
-    model4 = RandomForestClassifier(n_estimators=50, n_jobs=-1, max_features=mf)
-    model4.fit(X_train, y_train)
-    print("\nFor max_features={}".format(mf))
-    print(model4.score(X_train, y_train))
-    print(model4.score(X_val, y_val))
-    
- 
-
-# Using min_sample_split
-# It holds the minimum sample required to split
-for ms in (10,20,30,40,50,60,70,80,90,100):
-    model5 = RandomForestClassifier(n_estimators=50, n_jobs=-1, min_samples_split=ms, bootstrap=False)
-    model5.fit(X_train, y_train)
-    print("\nFor min_sample={}".format(ms))
-    print(model5.score(X_train, y_train))
-    print(model5.score(X_val, y_val))
-
-
-#min_sample_leaf
-#min_impurity_decrease  
-#Bootstrap
-
-model6=DecisionTreeClassifier(random_state=42, min_samples_leaf=50)
-model6.fit(X_train, y_train)
-print(model6.score(X_train, y_train))
-print(model6.score(X_val, y_val))
-
-# Final model with all hyperparameters
-from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import RandomizedSearchCV
-import warnings
-warnings.filterwarnings('ignore')
-final_model = RandomForestClassifier(random_state=42)
 param={
     'n_estimators':[30,60,90],
-    'criterion':['gini','entropy', 'log_loss'],
+    'criterion':['gini','entropy','log_loss'],
     'max_depth':[1,2,3,4,5],
-    'max_features':[None,'sqrt','log2'],
+    'max_features':['sqrt','log2'],
     'min_samples_split':[50,60,70,80,90,100],
     'max_leaf_nodes':[40,50,60,70,80,90,100]
 }
 
-# Using grid_search_cv
-#grid = GridSearchCV(estimator=final_model, param_grid=param, scoring=accuracy_score, n_jobs=-1, cv=5)
-grid = RandomizedSearchCV(n_iter=500, estimator=final_model, param_distributions=param, scoring=accuracy_score, n_jobs=-1, cv=5)
+grid = RandomizedSearchCV(n_iter=500, estimator=RandomForestClassifier(random_state=42), param_distributions=param, scoring='accuracy', n_jobs=-1, cv=5)
 grid.fit(X_train, y_train)
-print(grid.best_params_)
 
-grid.best_score_
+print("The best params chosen are {}".fomrat(grid.best_params_))
+print("The best score is {}".fomrat(grid.best_score_))
 
-#n_estimators': 60, 'min_samples_split': 100, 'max_leaf_nodes': 90, 'max_features': 'log2', 'max_depth': 3, 'criterion': 'entropy'
-m = RandomForestClassifier(n_estimators=60, min_samples_split=100, max_leaf_nodes=90, max_features='log2', max_depth=3, criterion='entropy')
-m.fit(X_train, y_train)
+# %%
+# Best model so far with best_params
+best_params=grid.best_params_
+final_model = RandomForestClassifier(n_estimators=best_params['n_estimators'],
+    criterion=best_params['criterion'],
+    max_depth=best_params['max_depth'],
+    max_features=best_params['max_features'],
+    min_samples_split=best_params['min_samples_split'],
+    max_leaf_nodes=best_params['max_leaf_nodes'],
+    random_state=42)
+final_model.fit(X_train, y_train)
+y_pred=final_model.predict(X_test)
 
-y_pred=m.predict(X_test)
-accuracy_score(y_test, y_pred)
+final_model_acc, final_model_pre, final_model_f1 = print_metric(y_test, y_pred)
+cnf=confusion_matrix(y_test, y_pred)
 
-# Decision Tree Ended
+plt.figure(figsize=(6, 4))
+sns.heatmap(cnf, annot=True, fmt="d", cmap="Blues", cbar=False, 
+            xticklabels=["Predicted 0", "Predicted 1"], 
+            yticklabels=["Actual 0", "Actual 1"])
+plt.title("Confusion Matrix")
+plt.show()
+
+# ROC Curve
+fpr, tpr, thresholds = roc_curve(y_test, y_pred)
+roc_auc = auc(fpr, tpr)
+plt.figure(figsize=(8, 6))
+plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC Curve (AUC = {roc_auc:.2f})')
+plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic (ROC) Curve')
+plt.legend(loc="lower right")
+plt.show()
+
+# Precision-Recall Curve
+precision, recall, thresholds = precision_recall_curve(y_test, y_pred)
+avg_precision = average_precision_score(y_test, y_pred)
+plt.figure(figsize=(8, 6))
+plt.plot(recall, precision, color='darkorange', lw=2, label=f'Precision-Recall Curve (AP = {avg_precision:.2f})')
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.title('Precision-Recall Curve')
+plt.legend(loc="lower right")
+plt.show()
 
